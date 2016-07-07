dcfcc180e945d935da315fba72ef77ec3c95d1e0
Ismael de Esteban <ismael@feverup.com>
2016-07-08 00:11:45 +0200

    Adding people tab

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
+    url(r'^landing/', include('landing.urls', namespace='landing')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- landing/urls.py
+++ landing/urls.py
@@ -3,4 +3,16 @@ try:
 except ImportError:  # django < 1.4
     from django.conf.urls.defaults import *
 
+from django.views.generic import TemplateView
+
 # place app url patterns here
+
+
+urlpatterns = [
+    # url pattern for the userlistview
+    url(
+        regex=r'^people/$',
+        view=TemplateView.as_view(template_name='landing/people.html'),
+        name='people'
+    ),
+]
