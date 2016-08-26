ec04cc06c5b9bc8982366f054f1976e97fe4ac83
Ismael de Esteban <ismael@feverup.com>
2016-08-26 14:41:23 +0200

    Updating GEOIP path for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -212,6 +212,6 @@ RAVEN_CONFIG = {
 # Custom Admin URL, use {% url 'admin:index' %}
 ADMIN_URL = env('DJANGO_ADMIN_URL')
 
-GEOIP_PATH = '/app/GeoLiteCity.dat'
+GEOIP_PATH = 'vendor/'
 
 # Your production stuff: Below this line define 3rd party library settings
