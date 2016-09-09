f3c35fb1a4b54ddeb984e1963d645323bbdc1acb
pilamb <perikopalotes@gmail.com>
2016-09-09 19:37:35 +0200

    improving the model

--- board/models.py
+++ board/models.py
@@ -1,34 +1,41 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals, absolute_import
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
-from users.models import User
 
 
 @python_2_unicode_compatible
-class Post(models.Model):
+class Service(models.Model):
     """
     Represents an advert of a user on the platform. Its intended to be
     exchangeable for seeds and mainly geolocated oriented (TODOs!)
     """
 
-    date = models.DateTimeField(auto_now_created=True)
-    text = models.TextField(max_length=2000)
-    date_range = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)  # posting date
+    text = models.TextField(max_length=2000)  # description of the service
+    period = models.DurationField(blank=False)
+    start_hour = models.TimeField(blank=True,)  # is offered
+    end_hour = models.TimeField()  # might be optional
+
     # TODO: gps intentional: map = models.¿?
-    # TODO: seeds ammount
+    # TODO: seeds ammount - bucket
     # TODO: photomanager album 1-N relationship
-    pic = models.ImageField()
-    # TODO: https://github.com/zsiciarz/django-pgallery/blob/master/pgallery/models.py#L90
+
+    pic = models.ImageField(upload_to=None, height_field=None, width_field=
+                            None, max_length=100, **options)
+    #  TODO: https://github.com/zsiciarz/django-pgallery/blob/master/pgallery
+    # /models.py#L90   <- a cool image manager django app
+
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
 
-    def __str__(self):
-        return self.username
+#    def __str__(self):
+#        return u'something goes here'
 
 #    def get_absolute_url(self):
 #        return reverse('users:detail', kwargs={'username': self.username})
