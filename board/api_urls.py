e617187fab79e58b1d9d5bfa1fc5ac1cb57eba26
pilamb <perikopalotes@gmail.com>
2016-09-09 21:02:29 +0200

    seralizing and improving Service app

---
+++ board/api_urls.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # URL pattern for the UserListView
+    url(
+        regex=r'^$',
+        view=views.UserList.as_view(),
+        name='list'
+    ),
+
+    # URL pattern for the UserDetailView
+    url(
+        regex=r'^(?P<pk>[0-9]+)/$',
+        view=views.UserDetail.as_view(),
+        name='detail'
+    ),
+
+
+]
