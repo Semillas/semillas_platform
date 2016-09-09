042e8b31372f001ae6d6d38e70ea910b43077fbd
pilamb <perikopalotes@gmail.com>
2016-09-09 21:02:43 +0200

    seralizing and improving Service app

--- board/api_urls.py
+++ board/api_urls.py
@@ -6,17 +6,17 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # URL pattern for the UserListView
+    # URL pattern for the ServiceListView
     url(
         regex=r'^$',
-        view=views.UserList.as_view(),
+        view=views.ServiceList.as_view(),
         name='list'
     ),
 
-    # URL pattern for the UserDetailView
+    # URL pattern for the ServiceDetailView
     url(
         regex=r'^(?P<pk>[0-9]+)/$',
-        view=views.UserDetail.as_view(),
+        view=views.ServiceDetail.as_view(),
         name='detail'
     ),
 
--- board/views.py
+++ board/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
-# Create your views here.
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, ListView, RedirectView, UpdateView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from rest_framework import generics
+from rest_framework import permissions
+
+from .models import Service
+from .serializers import ServiceSerializer
+
+
+class ServiceDetailView(LoginRequiredMixin, DetailView):
+    model = Service
+    # These next two lines tell the view to index lookups by username
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+
+
+class ServiceRedirectView(LoginRequiredMixin, RedirectView):
+    permanent = False
+
+    def get_redirect_url(self):
+        return reverse('users:detail',
+                       kwargs={'username': self.request.user.username})
+
+
+class ServiceUpdateView(LoginRequiredMixin, UpdateView):
+    fields = ['name', ]
+    model = Service
+
+    def get_success_url(self):
+        return reverse('users:detail',
+                       kwargs={'username': self.request.user.username})
+
+    def get_object(self):
+        # Only get the User record for the user making the request
+        return Service.objects.get(username=self.request.user.username)
+
+
+class ServiceListView(LoginRequiredMixin, ListView):
+    model = Service
+    # These next two lines tell the view to index lookups by username
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+
+
+class ServiceList(generics.ListAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class ServiceDetail(generics.RetrieveUpdateAPIView):
+    """ access: curl http://0.0.0.0:8000/api/v1/user/2/
+    """
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = (permissions.IsAuthenticated,)
