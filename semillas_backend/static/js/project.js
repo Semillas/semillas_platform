5745ef579edfecd02686c6155ba10ba7c578a654
pilamb <perikopalotes@gmail.com>
2016-09-16 21:35:38 +0200

    a new CSS and JS way to go to the top with a smooth manner when clicking the up/subir go up button

--- semillas_backend/static/js/project.js
+++ semillas_backend/static/js/project.js
@@ -1,1 +1,20 @@
 /* Project specific Javascript goes here. */
+
+/* Up button JS */
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
