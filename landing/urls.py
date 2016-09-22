017b55696ad9b14a4245bc2b2d570f84be7d6dd2
Ismael de Esteban <ismael@feverup.com>
2016-09-22 16:54:53 +0200

    Removing unused landing sections

--- landing/urls.py
+++ landing/urls.py
@@ -11,26 +11,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     # url pattern for the userlistview
     url(
-        regex=r'^terms/$',
-        view=TemplateView.as_view(template_name='landing/terms.html'),
-        name='terms'
-    ),
-    url(
-        regex=r'^privacy/$',
-        view=TemplateView.as_view(template_name='landing/privacy.html'),
-        name='privacy'
-    ),
-    url(
-        regex=r'^license/$',
-        view=TemplateView.as_view(template_name='landing/license.html'),
-        name='license'
-    ),
-    url(
-        regex=r'^about/$',
-        view=TemplateView.as_view(template_name='landing/about.html'),
-        name='about'
-    ),
-    url(
         regex=r'^FAQ/$',
         view=TemplateView.as_view(template_name='landing/FAQ.html'),
         name='FAQ'
