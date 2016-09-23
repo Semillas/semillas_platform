d547b7e8745d2b6baea5f46470be933303341705
Ismael de Esteban <ismael@feverup.com>
2016-09-23 12:39:34 +0200

    location and profile picture blank=True

--- semillas_backend/users/models.py
+++ semillas_backend/users/models.py
@@ -15,8 +15,8 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
-    location = PointField(null=True)
-    picture = models.ImageField(null=True)
+    location = PointField(null=True, blank=True)
+    picture = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.username
