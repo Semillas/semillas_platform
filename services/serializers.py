7e7e5258efd546d915de96c8d05ae1c106a7c8cd
Ismael de Esteban <ismael.deesteban@gmail.com>
2016-10-09 07:49:28 +0200

    Add photos to services

--- services/serializers.py
+++ services/serializers.py
@@ -1,15 +1,26 @@
 from rest_framework import serializers
 from .models import Service
+from .models import ServicePhoto
 
+
+class ServicePhotoSerializer(serializers.ModelSerializer):
+    """
+    """
+    class Meta:
+        model = ServicePhoto
+        fields = ('id', 'photo')
 
 
 class ServiceSerializer(serializers.ModelSerializer):
     """ Usage:
         from rest_framework.renderers import JSONRenderer
         from semillas_backend.users.serializers import UserSerializer
- 
+
         JSONRenderer().render(UserSerializer(user_instance).data)
     """
+
+    photos = ServicePhotoSerializer(many=True)
+
     class Meta:
         model = Service
-        fields = ('id', 'title', 'date', 'description')
+        fields = ('id', 'title', 'date', 'description', 'photos')
