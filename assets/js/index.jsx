a995ab74dc3830faa118160ef7cc3661c78edad7
Ismael de Esteban <ismael@feverup.com>
2016-10-28 16:33:09 +0200

    Skeleton with react router made

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import App from './app';
+import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
+
+import User from './modules/User'
+import Feed from './modules/Feed'
+import App from './modules/App'
+import About from './modules/About'
 
 const dest = document.getElementById('react-app');
 
-ReactDom.render(
-        <App/>,
-    dest)
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <Route path="/user" component={User}/>
+      <Route path="/about" component={About}/>
+    </Route>
+  </Router>
+), dest)
