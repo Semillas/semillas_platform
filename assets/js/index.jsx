e59033e333b7f4c4f53af9f32bcf3849b49a54c4
Ismael de Esteban <ismael@feverup.com>
2016-11-05 21:54:49 +0100

    Add Home to webapp navigation

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory, IndexRoute} from 'react-router'
 
 import User from './modules/User'
 import Feed from './modules/Feed'
@@ -14,6 +14,7 @@ const dest = document.getElementById('react-app');
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
+      <IndexRoute component={Feed}/>
       <Route path="/user/:id" component={User}/>
       <Route path="/about" component={About}/>
       <Route path="/service/:id" component={Service}/>
--- assets/js/modules/App.jsx
+++ assets/js/modules/App.jsx
@@ -5,8 +5,9 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h1>React Router Tutorial</h1>
+        <h1>Semillas</h1>
         <ul role="nav">
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/profile">Profile</NavLink></li>
           <li><NavLink to="/service/1">Service</NavLink></li>
