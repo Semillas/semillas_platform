49671a118ef7de2d4612f46ae1e8a6344ed12408
IgnaciodeNuevo <ignaciodenuevo@gmail.com>
2016-12-19 14:34:31 +0100

    Added Original Photos, Nav Styles and created empty nav templates

--- landing/urls.py
+++ landing/urls.py
@@ -10,19 +10,22 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     # url pattern for the userlistview
+
+
+  
+    url(
+        regex=r'^philosophy/$',
+        view=TemplateView.as_view(template_name='landing/philosophy.html'),
+        name='philosophy'
+    ),
+    url(
+        regex=r'^product/$',
+        view=TemplateView.as_view(template_name='landing/product_and_code.html'),
+        name='product'
+    ),
     url(
         regex=r'^people/$',
         view=TemplateView.as_view(template_name='landing/who_we_are.html'),
         name='people'
-    ),
-    url(
-        regex=r'^contact/$',
-        view=TemplateView.as_view(template_name='landing/contact.html'),
-        name='contact'
-    ),
-    url(
-        regex=r'^development/$',
-        view=TemplateView.as_view(template_name='landing/development.html'),
-        name='development'
     ),
 ]
