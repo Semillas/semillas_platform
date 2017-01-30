f26d44461612a673a3a55fb0d316bf0fc2aea954
Sergio Martinez <sergimartnez@yahoo.es>
2017-01-30 18:29:51 +0100

    Adding changes on serializer for view UserWalletDetail

--- wallet/serializers.py
+++ wallet/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from .models import Wallet, Transaction
 from semillas_backend.users.serializers import UserSerializer
 
-
 class CreateWalletSerializer(serializers.ModelSerializer):
 	""" Usage:
 	"""
@@ -14,25 +13,56 @@ class CreateWalletSerializer(serializers.ModelSerializer):
 class TransactionSerializer(CreateWalletSerializer):
 	user = serializers.SerializerMethodField()
 	balance = serializers.SerializerMethodField()
+	trans_value = serializers.SerializerMethodField()
 	class Meta:
 		model = Transaction
-		fields = ('id', 'value', 'balance', 'user', 'created_at')
+		fields = ('id', 'trans_value', 'balance', 'user', 'created_at')
 
 	def get_user(self, obj):
 		# import ipdb;ipdb.set_trace()
 		# is_owner = bool(self.context['wallet'].owner == obj.wallet_source.owner)
 		# if is_owner:
-		return obj.wallet_dest.owner.name
-		# else:
-		#     return obj.wallet_source.owner.name
+		if type(self.context['view']).__name__ ==  "UserWalletDetail":
+			owner_uuid = self.context['view'].kwargs['owner_uuid']
+			is_owner = bool(owner_uuid == str(obj.wallet_source.owner.uuid))
+			# import ipdb;ipdb.set_trace()
+			if is_owner:
+				return obj.wallet_dest.owner.name
+			else:
+				return obj.wallet_source.owner.name
+		else:
+			return obj.wallet_source.owner.name
+
 
 	def get_balance(self, obj):
 		# import ipdb;ipdb.set_trace()
 		# is_owner = bool(self.context['wallet'].owner == obj.wallet_source.owner)
 		# if is_owner:
-		return obj.balance_source
+		# return obj.balance_source
 		# else:
 		#     return obj.balance_dest
+		if type(self.context['view']).__name__ ==  "UserWalletDetail":
+			owner_uuid = self.context['view'].kwargs['owner_uuid']
+			is_owner = bool(owner_uuid == str(obj.wallet_source.owner.uuid))
+			# import ipdb;ipdb.set_trace()
+			if is_owner:
+				return obj.balance_source
+			else:
+				return obj.balance_dest
+		else:
+			return obj.balance_source
+
+	def get_trans_value(self, obj):
+		if type(self.context['view']).__name__ ==  "UserWalletDetail":
+			owner_uuid = self.context['view'].kwargs['owner_uuid']
+			is_owner = bool(owner_uuid == str(obj.wallet_source.owner.uuid))
+			# import ipdb;ipdb.set_trace()
+			if is_owner:
+				return -obj.value
+			else:
+				return obj.value
+		else:
+			return obj.value
 
 class WalletSerializer(CreateWalletSerializer):
 	""" Usage:
