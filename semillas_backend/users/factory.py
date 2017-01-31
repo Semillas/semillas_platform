9c3a0541fb3c8f01f9ed8a7cd1d7b556ea43c26c
Sergio Martinez <sergimartnez@yahoo.es>
2017-01-31 13:31:59 +0100

    Changin users factory to solve some codacy warnings

--- semillas_backend/users/factory.py
+++ semillas_backend/users/factory.py
@@ -5,13 +5,13 @@ from wallet.factory import WalletFactory
 from .models import User
 
 class UserFactory(factory.django.DjangoModelFactory):
-	class Meta:
-		model = User  # Equivalent to ``model = myapp.models.User``
-		#django_get_or_create = ('username',)
+    class Meta:
+        model = User  # Equivalent to ``model = myapp.models.User``
+        #django_get_or_create = ('username',)
 
-	name = factory.Faker('name')
-	email = factory.Faker('email')
-	username = factory.Faker('name')
-	location = Point(-3.7035285, 40.4169473) # Puerta del Sol, Madrid
-	picture = factory.django.ImageField()
-	wallet = factory.RelatedFactory(WalletFactory, 'owner')
+    name = factory.Faker('name')
+    email = factory.Faker('email')
+    username = factory.Faker('name')
+    location = Point(-3.7035285, 40.4169473) # Puerta del Sol, Madrid
+    picture = factory.django.ImageField()
+    wallet = factory.RelatedFactory(WalletFactory, 'owner')
