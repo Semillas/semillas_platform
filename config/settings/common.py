24491f43ce04b04a2fa4f82f88d6cb80e6a4e95a
Ismael de Esteban <ismael@feverup.com>
2017-02-24 23:37:32 +0100

    Using just Token Authentication

--- config/settings/common.py
+++ config/settings/common.py
@@ -316,7 +316,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
