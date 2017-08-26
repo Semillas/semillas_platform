9eda51d75e0559ad4bcb733c02e2678268f70e0f
Ismael de Esteban <ismael.deesteban@gmail.com>
2017-08-27 00:29:44 +0200

    Try to change environment variable just for daphne

--- chat/asgi.py
+++ chat/asgi.py
@@ -1,5 +1,5 @@
 import os
 import channels.asgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.chat")
+os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.chat"
 channel_layer = channels.asgi.get_channel_layer()
